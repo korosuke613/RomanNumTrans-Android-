d0bb13ac4d042cd58e0180683bd38455c7cb833c
Futa Hirakoba <korosuke613613@gmail.com>
2016-07-09 16:53:37 +0900

    Create: Beeno Activity.

---
+++ java/com/futa/romannumtrans/BeenoActivity.java
@@ -0,0 +1,16 @@
+package com.futa.romannumtrans;
+
+//      Intent intent = new Intent(this, BeenoActivity.class);
+//      startActivity(intent);
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeenoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beeno);
+    }
+}
